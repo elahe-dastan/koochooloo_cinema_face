394da589646debc9e99acbc72b533f496b910568
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-21 20:23:11 +0430

    sign up

--- components/User.vue
+++ components/User.vue
@@ -1,6 +1,12 @@
 <template>
   <form @submit.prevent="OnSubmit">
-    <input type="text" v-model="text" placeholder="username">
+    <input type="text" v-model="username" placeholder="username">
+    <input type="text" v-model="password" placeholder="password">
+    <input type="text" v-model="first_name" placeholder="first name">
+    <input type="text" v-model="last_name" placeholder="last name">
+    <input type="text" v-model="email" placeholder="email">
+    <input type="text" v-model="phone" placeholder="phone">
+    <input type="text" v-model="national_number" placeholder="national number">
     <input type="submit" value="submit">
   </form>
 </template>
@@ -12,13 +18,20 @@ export default {
   components: {Index},
   data() {
     return {
-      text: ''
+      username: '',
+      password: '',
+      first_name: '',
+      last_name: '',
+      email: '',
+      phone: '',
+      national_number: ''
     }
   },
   methods: {
     OnSubmit() {
-      this.$emit('username-text', this.text);
-      this.text = "";
+      this.$emit('signup', this.username, this.password, this.first_name, this.last_name, this.email, this.phone,
+      this.national_number);
+      this.username = "";
     }
   }
 }
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <User v-on:username-text="usernameText"></User>
+    <User v-on:signup="signup"></User>
   </div>
 </template>
 
@@ -14,9 +14,15 @@ export default {
     User
   },
   methods: {
-    async usernameText(text) {
+    async signup(username, password,first_name,last_name,email,phone, national_number) {
       await axios.post(`http://localhost:1373/api/signup`, {
-        username: text
+        username: username,
+        password: password,
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        phone: phone,
+        national_number: national_number
       })
         .then(function (response) {
           console.log(response);
